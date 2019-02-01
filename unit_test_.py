431bf3c133ffa868690ebd925e37f218fe57b759
kdani99 <dkay@deeplearning.ai>
2019-02-01 13:09:06 -0800

    add unit tests

---
+++ unit_test_.py
@@ -0,0 +1,17 @@
+from lambda_function import parse_imageset_meta, parse_image_meta
+import unittest
+
+class SimpleTest(unittest.TestCase): 
+ 
+    def test_parse_imageset_meta(self):         
+        self.assertTrue(True)
+
+    def test_parse_image_meta(self):         
+        self.assertTrue(True) 
+
+    def test_generate_npz(self):
+    	self.assertTrue(True)         
+  
+
+if __name__ == '__main__': 
+    unittest.main() 
